b226424c3951b769ade00630e27a15052a63a8a9
Masaki Murase <murasemasaki_0216@yahoo.co.jp>
2015-09-16 12:34:34 +0900

    add: 道路の色の種類を追加

--- src/servlet/DrawElasticRoad.java
+++ src/servlet/DrawElasticRoad.java
@@ -198,6 +198,16 @@ public class DrawElasticRoad {
 		ArrayList<ArrayList<Point2D>> orangePath = new ArrayList<>();	// 一般地方道.
 		ArrayList<ArrayList<Point2D>> yellowPath = new ArrayList<>();	// 一般道(2車線以上).
 		ArrayList<ArrayList<Point2D>> whitePath = new ArrayList<>();	// その他.
+		
+
+		ArrayList<ArrayList<Point2D>> narrowWhitePath = new ArrayList<>();
+		ArrayList<ArrayList<Point2D>> grayPath = new ArrayList<>();
+		ArrayList<ArrayList<Point2D>> faintGrayPath = new ArrayList<>();
+		ArrayList<ArrayList<Point2D>> brownDashPath = new ArrayList<>();
+		
+		ArrayList<ArrayList<Point2D>> blueDashPath = new ArrayList<>();
+		ArrayList<ArrayList<Point2D>> redDashPath = new ArrayList<>();
+		ArrayList<ArrayList<Point2D>> wideRedDashPath = new ArrayList<>();
 		ArrayList<ArrayList<Point2D>> railPath = new ArrayList<>();		// 電車.
 		ArrayList<ArrayList<Point2D>> subwayPath = new ArrayList<>();	// 地下鉄.
 		
@@ -229,10 +239,14 @@ public class DrawElasticRoad {
 //				paint2dLine(new Line2D.Double(p1Xy, p2Xy), Color.pink, (float)3, __clazz.get(i));
 			}
 			
-			dispatchLineStyle(path, __clazz.get(i), bluePath, greenPath, redPath, orangePath, yellowPath, whitePath, railPath, subwayPath);
+			dispatchLineStyle(path, __clazz.get(i), bluePath, greenPath, redPath, orangePath, yellowPath, whitePath,
+					narrowWhitePath, grayPath, faintGrayPath, brownDashPath,
+					blueDashPath, redDashPath, wideRedDashPath, railPath, subwayPath);
 		}
 		
-		paintEachLineStyle(bluePath, greenPath, redPath, orangePath, yellowPath, whitePath, railPath, subwayPath);
+		paintEachLineStyle(bluePath, greenPath, redPath, orangePath, yellowPath, whitePath,
+				narrowWhitePath, grayPath, faintGrayPath, brownDashPath,
+				blueDashPath, redDashPath, wideRedDashPath, railPath, subwayPath);
 	}
 
 	/**
@@ -247,45 +261,67 @@ public class DrawElasticRoad {
 			ArrayList<ArrayList<Point2D>> orangePath, 
 			ArrayList<ArrayList<Point2D>> yellowPath,
 			ArrayList<ArrayList<Point2D>> whitePath,
+			ArrayList<ArrayList<Point2D>> narrowWhitePath,
+			ArrayList<ArrayList<Point2D>> grayPath,
+			ArrayList<ArrayList<Point2D>> faintGrayPath,
+			ArrayList<ArrayList<Point2D>> brownDashPath,
+			ArrayList<ArrayList<Point2D>> blueDashPath,
+			ArrayList<ArrayList<Point2D>> redDashPath,
+			ArrayList<ArrayList<Point2D>> wideRedDashPath,
 			ArrayList<ArrayList<Point2D>> railPath,
 			ArrayList<ArrayList<Point2D>> subwayPath
 			){
 
 		switch(__clazz){
-			// 水色.
-			case 11:
+			case 11:// 水色(mortorway,mortorwa_link).
 			case 12:
 				bluePath.add(path);
 				break;
-				//緑.
-			case 13:
+			case 13://緑(trunk, trunk_link).
 			case 14:
 				greenPath.add(path);
 				break;
-				// 赤.
-			case 15:
+			case 15:// 赤 (primary, primary_link).
 			case 16:
 				redPath.add(path);
 				break;
-				// オレンジ.
-			case 21:
+			case 21:// オレンジ (secondary, secondary_link).
 			case 22:
 				orangePath.add(path);
 				break;
-				// 黄色.
-			case 31:
+			case 31:// 黄色 (tertiary).
 				yellowPath.add(path);
 				break;
-				// 白.
-			case 32:
+			case 32:// 白 (residential, road, unclassified).
 			case 41:
 			case 42:
-			case 51:
-			case 63:
 				whitePath.add(path);
-				break;			
-				// 鉄道 白黒.
-			case 3:
+				break;
+			case 51://(service)
+				narrowWhitePath.add(path);
+				break;
+			case 63:// (living street).
+				grayPath.add(path);
+				break;
+			case 62://(pedestrian)
+				faintGrayPath.add(path);
+				break;
+			case 71:// (track).
+				brownDashPath.add(path);
+				break;
+			case 72://(path).
+				whitePath.add(path);
+				break;
+			case 81://青点線＋白線(cycleway)
+				blueDashPath.add(path);
+				break;
+			case 91:// 赤点線(footway).
+				redDashPath.add(path);
+				break;
+			case 92:// 太め赤白点線(step).
+				wideRedDashPath.add(path);
+				break;
+			case 3:// 鉄道 白黒.
 				railPath.add(path);
 				break;
 				// 地下鉄.
@@ -314,11 +350,52 @@ public class DrawElasticRoad {
 			ArrayList<ArrayList<Point2D>> orangePath, 
 			ArrayList<ArrayList<Point2D>> yellowPath,
 			ArrayList<ArrayList<Point2D>> whitePath,
+			
+			ArrayList<ArrayList<Point2D>> narrowWhitePath,
+			ArrayList<ArrayList<Point2D>> grayPath,
+			ArrayList<ArrayList<Point2D>> faintGrayPath,
+			ArrayList<ArrayList<Point2D>> brownDashPath,
+			
+			ArrayList<ArrayList<Point2D>> blueDashPath,
+			ArrayList<ArrayList<Point2D>> redDashPath,
+			ArrayList<ArrayList<Point2D>> wideRedDashPath,
+			
 			ArrayList<ArrayList<Point2D>> railPath,
 			ArrayList<ArrayList<Point2D>> subwayPath
 		){
 		// 道路クラスごとに描画.
 		// 最初に描画する道路が後ろに来る.
+		for(ArrayList<Point2D> line: wideRedDashPath){
+			float[] dash = { 2.f, 2.f };
+			paintPath(line, 3, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(252, 127, 111), (float) 1., dash, (float) 0.);
+		}
+		for(ArrayList<Point2D> line: redDashPath){
+			float[] dash = { 2.f, 2.f };
+			paintPath(line, 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(252, 127, 111), (float) 1., dash, (float) 0.);
+		}
+		for(ArrayList<Point2D> line: blueDashPath){
+			float[] dash = { 4.f, 4.f };
+			paintPath(line, 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(0, 0, 255), (float) 1., dash, (float) 0.);
+		}
+		
+		
+		for(ArrayList<Point2D> line: brownDashPath){
+			float[] dash = { 3.f, 3.f };
+			paintPath(line, 1, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(164, 134, 46), (float) 1., dash, (float) 0.);
+		}
+		for(ArrayList<Point2D> line: faintGrayPath){
+			paintPath(line, 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(189,189,189));// 枠.
+			paintPath(line, 3, BasicStroke.JOIN_ROUND, BasicStroke.JOIN_ROUND, new Color(237,237,237));// 中の線.
+		}
+		for(ArrayList<Point2D> line: grayPath){
+			paintPath(line, 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(255,255,255));// 枠.
+			paintPath(line, 3, BasicStroke.JOIN_ROUND, BasicStroke.JOIN_ROUND, new Color(204,204,204));// 中の線.
+		}
+		for(ArrayList<Point2D> line: narrowWhitePath){
+			paintPath(line, 4, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(189,189,188));// 枠.
+			paintPath(line, 3, BasicStroke.JOIN_ROUND, BasicStroke.JOIN_ROUND, new Color(255,255,255));// 中の線.
+		}
+		
 		for(ArrayList<Point2D> whiteLine: whitePath){
 			paintPath(whiteLine, 8, BasicStroke.CAP_BUTT, BasicStroke.JOIN_BEVEL, new Color(189,189,188));// 枠.
 			paintPath(whiteLine, 7, BasicStroke.JOIN_ROUND, BasicStroke.JOIN_ROUND, new Color(255,255,255));// 中の線.
